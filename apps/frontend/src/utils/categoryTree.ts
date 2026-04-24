5943fb9d7cb52bf949e907e27e14b0dfaac2a87d
hltav <hudsonlimatavares@gmail.com>
2026-04-24 12:03:43 -0300

    Removing the getAllCategoryIdsFromTree function

--- apps/frontend/src/utils/categoryTree.ts
+++
@@ -1,8 +0,0 @@
-import type { CategoryTree } from "../types";
-
-export function getAllCategoryIdsFromTree(
-  tree: CategoryTree,
-  parentName: string,
-): number[] {
-  return tree[parentName]?.map((c) => c.id) ?? [];
-}
